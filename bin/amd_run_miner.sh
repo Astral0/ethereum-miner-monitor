a7144f09d4b167f45b759ba476a652a1ce1c6ad5
Astral <astral0@protonmail.com>
2021-02-10 22:01:37 +0000

    minor

--- bin/amd_run_miner.sh
+++ bin/amd_run_miner.sh
@@ -5,8 +5,8 @@ if [[ `whoami` = 'root' ]]; then
   exit 1
 fi
 
-#screen -S miner -dm bash -c "cd /home/astral/PROJETS/PhoenixMiner_3.5d_Linux ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.stderr"
+#screen -S miner -dm bash -c "cd /home/astral/PROJETS/PhoenixMiner_3.5d_Linux ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.log"
 
-screen -S miner -dm bash -c "cd /home/astral/PROJETS/teamredminer-v0.8.0-linux/ ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.stderr"
+screen -S miner -dm bash -c "cd /home/astral/PROJETS/teamredminer-v0.8.0-linux/ ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.log"
 
-#screen -S miner -dm bash -c "cd /home/astral/PROJETS/ethminer-0.18.0-cuda-9/ ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.stderr"
+#screen -S miner -dm bash -c "cd /home/astral/PROJETS/ethminer-0.18.0-cuda-9/ ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.log"
--- bin/nvidia_run_miner.sh
+++ bin/nvidia_run_miner.sh
@@ -6,8 +6,8 @@ if [[ `whoami` = 'root' ]]; then
 fi
 
 
-#screen -S miner -dm bash -c "cd /home/astral/PROJETS/PhoenixMiner_3.5d_Linux ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.stderr"
+#screen -S miner -dm bash -c "cd /home/astral/PROJETS/PhoenixMiner_3.5d_Linux ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.log"
 
-#screen -S miner -dm bash -c "cd /home/astral/PROJETS/t-rex-0.19.9-linux-cuda10.0 ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.stderr"
+#screen -S miner -dm bash -c "cd /home/astral/PROJETS/t-rex-0.19.9-linux-cuda10.0 ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.log"
 
-screen -S miner -dm bash -c "cd /home/astral/PROJETS/ethminer-0.18.0-cuda-9 ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.stderr"
+screen -S miner -dm bash -c "cd /home/astral/PROJETS/ethminer-0.18.0-cuda-9 ; ./start_miner.sh >/var/log/miner/miner.log 2>/var/log/miner/miner.log"
